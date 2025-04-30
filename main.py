dcac069d4044e153d3e6bff21d8b264b3f46b925
Lallindo <brunolallo8@gmail.com@>
2025-04-30 14:24:33 -0300

    Estudo sobre APIs em python usando FastAPI, SQLModel e Alembic

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, HTTPException, Path, Query, Depends
+from models import BandCreate, Band, GenreURLChoices, Album
+from typing import Annotated
+from sqlmodel import Session, select
+from db import get_session
+
+app = FastAPI()
+
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Eletronic'},
+    {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal', 'albums': [
+        {'title': 'Master of Reality', 'release_date': '1971-07-21'}    
+    ]},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
+    {'id': 5, 'name': 'Led Zeppelin', 'genre': 'Rock', 'albums': [
+        {'title': 'Led Zeppelin IV', 'release_date': '1971-11-08'}
+    ]}
+]
+
+@app.get('/bands')
+async def index(
+    genre: GenreURLChoices | None = None,
+    q: Annotated[str | None, Query(max_length=10)] = None, 
+    session: Session = Depends(get_session)
+) -> list[Band]:
+    band_list = session.exec(select(Band)).all()
+    
+    if genre:
+        band_list = [b for b in band_list if b.genre.value.lower() == genre.value]
+    
+    if q:
+        band_list = [
+            b for b in band_list if q.lower() in b.name.lower()
+        ]
+        
+    return band_list
+
+@app.get('/bands/{band_id}')
+async def get_band(
+    band_id: Annotated[int, Path(title="The band ID")], 
+    session: Session = Depends(get_session)
+) -> Band:
+    band = session.get(Band, band_id)
+    if band is None:
+        raise HTTPException(status_code=404, detail='Band not found')
+    return band
+
+@app.post('/bands')
+async def create_band(
+    band_data: BandCreate,
+    session: Session = Depends(get_session)
+) -> Band:
+    band = Band(name=band_data.name, genre=band_data.genre)
+    session.add(band)
+    
+    if band_data.albums:
+        for album in band_data.albums:
+            album_obj = Album(
+                title=album.title, 
+                release_date=album.release_date, 
+                band=band)
+            session.add(album_obj)
+    
+    session.commit()
+    session.refresh(band)
+    
+    return band
